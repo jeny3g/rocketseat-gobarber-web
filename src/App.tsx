6b5299d645d5e2577a23835b882f0571f887f8da
Jean Oliveira Miranda <mirandajean2009@gmail.com>
2021-01-09 12:52:59 -0300

    Estrutura de Toasts

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn';
 
 import GlobalStyle from './styles/global';
 
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
@@ -13,6 +14,7 @@ const App: React.FC = () => (
       <SignIn />
     </AuthProvider>
 
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
