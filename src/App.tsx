a01de997999c490d9ceb5b628e6cae7874853851
Jean Oliveira Miranda <mirandajean2009@gmail.com>
2021-01-09 12:13:42 -0300

    Logout da aplicaçao

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import SignIn from './pages/SignIn';
 
 import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
